489ff6848638290d2b84b2b5451bd450b8d647c2
DumpishSad <alex30495607@mail.ru>
2024-09-30 19:00:08 +0300

    add class/func_view

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import func_view, class_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('func_view/', func_view),
+    path('class_view/', class_view.as_view()),
 ]
